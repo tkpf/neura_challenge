f68bf04f767f5d981ed617699fe0d32c18a0cd18
tkpf <69465288+tkpf@users.noreply.github.com>
2023-05-06 23:26:27 +0200

    avoid import

--- scripts/stream_consumer.py
+++ scripts/stream_consumer.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
 
+from cv_bridge import CvBridge
+
+def detect_edges_in_image(img_in_cvformat):
+    bridge = CvBridge()
+    imgmsg = bridge.cv2_to_imgmsg(img_in_cvformat, encoding="bgr8")
+    print("File converted to cv format. Waiting for Service.")
+    rospy.wait_for_service("edge_detection")
+    print("Service found.")
+    try:
+        vision_processing_srv = rospy.ServiceProxy('edge_detection', EdgeDetection)
+        print("Calling Service...")
+        resp = vision_processing_srv(imgmsg)
+        print("Service response received.")
+        img_analyzed = bridge.imgmsg_to_cv2(resp.img)
+        return img_analyzed
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+
+
+
+
+
+
+
+
 import rospy
 import cv2 as cv
 from sensor_msgs.msg import Image
@@ -26,18 +51,3 @@ def listener():
 if __name__ == '__main__':
     listener()
 
-def detect_edges_in_image(img_in_cvformat):
-    bridge = CvBridge()
-    imgmsg = bridge.cv2_to_imgmsg(img_in_cvformat, encoding="bgr8")
-    print("File converted to cv format. Waiting for Service.")
-    rospy.wait_for_service("edge_detection")
-    print("Service found.")
-    try:
-        vision_processing_srv = rospy.ServiceProxy('edge_detection', EdgeDetection)
-        print("Calling Service...")
-        resp = vision_processing_srv(imgmsg)
-        print("Service response received.")
-        img_analyzed = bridge.imgmsg_to_cv2(resp.img)
-        return img_analyzed
-    except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
