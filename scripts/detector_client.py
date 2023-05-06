bcc6ca43b7180270692469d04f7d6e3b33b31efb
tkpf <69465288+tkpf@users.noreply.github.com>
2023-05-06 20:56:54 +0200

    renaming

---
+++ scripts/detector_client.py
@@ -0,0 +1,42 @@
+# !/usr/bin/env python
+
+import sys
+import rospy
+import cv2 as cv
+import numpy as np
+from cv_bridge import CvBridge
+from vision_processing.srv import FindEdgesInImage, FindEdgesInImageResponse
+
+def processImage(filepath):
+    img_in_cvformat = cv.imread(filepath)
+    print("File found!")
+    bridge = CvBridge()
+    img_in_matformat = bridge.cv2_to_imgmsg(img_in_cvformat, encoding="bgr8")
+    print(img_in_matformat.encoding)
+    print("File converted to cv format. Waiting for Service.")
+    rospy.wait_for_service("vision_processing_srv")
+    print("Service found.")
+    try:
+        vision_processing_srv = rospy.ServiceProxy('vision_processing_srv', FindEdgesInImage)
+        print("Calling Service...")
+        resp = vision_processing_srv(img_in_matformat)
+        print("Service response received.")
+        img_analyzed_in_cvformat = bridge.imgmsg_to_cv2(resp.imgOut)
+        return img_analyzed_in_cvformat
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        filepath = sys.argv[1]
+        print("Search for file at location %s.")
+    else:
+        print("To many arguments provided. Abort...")
+        sys.exit()
+    img = processImage(filepath)
+    print("Show Image...")
+    cv.imshow('Analyzed_Img',img)
+    if cv.waitKey(0) & 0xff == 27:
+        cv.destroyAllWindows()
