f931201b79e7d7358291ff2befd4f2a02b23b973
tkpf <69465288+tkpf@users.noreply.github.com>
2023-05-06 23:39:12 +0200

    fixing raw data

--- scripts/stream_consumer.py
+++ scripts/stream_consumer.py
@@ -3,9 +3,9 @@
 from cv_bridge import CvBridge
 from edge_detection.srv import EdgeDetection, EdgeDetectionResponse
 
-def detect_edges_in_image(imgmsg):
+def detect_edges_in_image(img_in_cvformat):
     bridge = CvBridge()
-    # imgmsg = bridge.cv2_to_imgmsg(img_in_cvformat, encoding="bgr8")
+    imgmsg = bridge.cv2_to_imgmsg(img_in_cvformat, encoding="bgr8")
     print("File converted to cv format. Waiting for Service.")
     rospy.wait_for_service("edge_detection")
     print("Service found.")
@@ -30,6 +30,7 @@ def detect_edges_in_image(imgmsg):
 import rospy
 import cv2 as cv
 from sensor_msgs.msg import Image
+import numpy as np
 
 # from detector_client import detect_edges_in_image
 
@@ -37,8 +38,10 @@ IMAGE_STREAM_TOPIC = '/camera/color/image_raw'
 
 def callback(data):
     #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    enriched_img = detect_edges_in_image(data.data)
-    cv.imshow('dst',enriched_img)
+    print(data)
+    img = np.frombuffer(data.data, dtype=np.uint8)
+    enriched_img = detect_edges_in_image(img)
+    cv.imshow('Analyzed Image',enriched_img)
     cv.waitKey(2000) # delay for 5000 ms (5 seconds)
     cv.destroyAllWindows()
 
