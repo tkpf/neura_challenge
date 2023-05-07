13388fea9da56431ef89032eb2692a800bcb46bd
tkpf <69465288+tkpf@users.noreply.github.com>
2023-05-07 10:42:13 +0200

    fixing raw data

--- scripts/stream_consumer.py
+++ scripts/stream_consumer.py
@@ -33,12 +33,15 @@ import numpy as np
 
 # from detector_client import detect_edges_in_image
 
-IMAGE_STREAM_TOPIC = '/camera/color/image_raw'
+IMAGE_STREAM_TOPIC = '/camera/color/image_raw/republish'
 
 def callback(data):
     #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
     
     img = np.frombuffer(data.data, dtype=np.uint8)
+    cv.imshow('Analyzed Image',img)
+    cv.waitKey(1000) # delay for 5000 ms (5 seconds)
+    cv.destroyAllWindows()
     enriched_img = detect_edges_in_image(img)
     cv.imshow('Analyzed Image',enriched_img)
     cv.waitKey(2000) # delay for 5000 ms (5 seconds)
