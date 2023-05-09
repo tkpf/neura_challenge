f541f8e2a67379fcb7cd3737b3b628c1c77f5cf6
tkpf <69465288+tkpf@users.noreply.github.com>
2023-05-09 10:37:03 +0200

    print to logging

--- edge_detection/scripts/edge_detector.py
+++ edge_detection/scripts/edge_detector.py
@@ -10,80 +10,68 @@ from sensor_msgs.msg import Image
 from edge_detection.srv import EdgeDetection, EdgeDetectionResponse
 
 
+class Edge_Detection_Service:
+    # init and start up service
+    def __init__(self):
+        self.cv_bridge = CvBridge()
+        self.startup()
 
-def edge_detection_service():
-    rospy.init_node("edge_detection_service")
-    s = rospy.Service("edge_detection", EdgeDetection, detection_service_callback)
-    print("Ready to process Image...")
-    rospy.spin()
+    def startup(self):
+        rospy.init_node("edge_detection_service")
+        rospy.loginfo("Starting Edge Detection Service...")
+        s = rospy.Service("edge_detection", EdgeDetection, self.detection_service_callback)
+        rospy.loginfo("Edge Detection Service up. Ready to process Image...")
+        rospy.spin()
 
-def detection_service_callback(req):
-    bridge = CvBridge()
-    img_in_cvformat = bridge.imgmsg_to_cv2(req.img)
-    processed_img_in_cvformat = detect_edges_canny(img_in_cvformat)
-    processed_imgmsg = bridge.cv2_to_imgmsg(processed_img_in_cvformat, encoding='rgb8')
-    # set header
-    print(f"This is the image header:\n{req.img.header}")
-    processed_imgmsg.header = req.img.header
-    return EdgeDetectionResponse(processed_imgmsg)
+    # TODO set encodings
+    def detection_service_callback(self, req):
+        rospy.logdebug("New edge detection callback received. Payload:\n" + req.img)
+        img_in_cvformat = self.bridge.imgmsg_to_cv2(req.img)
+        processed_img_in_cvformat = self.detect_edges_canny(img_in_cvformat)
+        processed_imgmsg = self.bridge.cv2_to_imgmsg(processed_img_in_cvformat, encoding='rgb8')
+        # set header
+        processed_imgmsg.header = req.img.header
+        return EdgeDetectionResponse(processed_imgmsg)
 
-def detect_edges(img):
-    # # Apply Gaussian Smoothing
-    # img =  cv.GaussianBlur(img, (5, 5), 0)
-    # Apply Bilateral smoothing
-    img = cv.bilateralFilter(img,5,100,100) # experiment more with parameteres, default values were 9, 75, 75
+    def detect_edges(self, img, gaussian_smoothing = False, bilateral_smoothing = True, dilate = True):
+        if gaussian_smoothing:
+            img =  cv.GaussianBlur(img, (5, 5), 0)
+        if bilateral_smoothing:
+            img = cv.bilateralFilter(img,5,100,100) # experiment more with parameteres, default values were 9, 75, 75
 
-    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+        gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+        gray = np.float32(gray)
 
-    gray = np.float32(gray)
-    dst = cv.cornerHarris(gray,3,5,0.04) # experiment with parameters, default values 2, 3, 0.04
-    print(np.shape(dst))
-    #print(dst>0.12*dst.max())
-    count = np.count_nonzero(dst>0.12*dst.max())
-    print(count)
-    #result is dilated for marking the corners, not important
-    dst = cv.dilate(dst,None)
+        dst = cv.cornerHarris(gray,3,5,0.04) # experiment with parameters, default values 2, 3, 0.04
+        print(np.shape(dst))
+        count = np.count_nonzero(dst>0.12*dst.max())
+        rospy.logdebug("Edges found in " + count +  " pixels.")
 
-    # Threshold for an optimal value, 0.1-0.2 seems to be a code choice, default value was 0.01
-    print(img)
-    img[dst>0.12*dst.max()]=[0,0,255]
-    return img
+        #result is dilated for marking the corners better, can be disregarded
+        if dilate:
+            dst = cv.dilate(dst,None)
 
+        # Threshold for an optimal value, 0.1-0.2 seems to be a code choice, default value was 0.01
+        img[dst>0.12*dst.max()]=[0,0,255]
+        return img
 
-def detect_edges_canny(img):
-    # Convert to grayscale
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # TODO color_brg2gray in all functions
+    def detect_edges_canny(self, img):
+        # Convert to grayscale
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        # Detect edges using Canny algorithm
+        edges = cv.Canny(gray, 100, 200)
+        # dilate to see
+        cv.dilate(edges,None)
+        # img is not writable, see img.flags e.g. by printing
+        img_writable = np.copy(img)
+        img_writable[edges > 0.2] = [0, 255, 0]
 
-    # Detect edges using Canny algorithm
-    edges = cv.Canny(gray, 100, 200)
-    # dilate to see
-    cv.dilate(edges,None)
-    # img is not writable, see img.flags e.g. by printing
-    img_writable = np.copy(img)
-    img_writable[edges > 0.2] = [0, 255, 0]
-
-    # Find contours of edges
-    contours, _ = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-
-    # Draw contours on original image in green color
-    #cv.drawContours(img, contours, -1, (0, 255, 0), 2)
-
-    
-
-    # # Iterate over contours and extract start and end points
-    # for contour in contours:
-    #     # Get the first and last point in the contour
-    #     start_point = tuple(contour[0][0])
-    #     end_point = tuple(contour[-1][0])
-    #     print(f"Start at {start_point}, End at {end_point}")
-
-    return img_writable
+        return img_writable
 
 
 if __name__ == '__main__':
-    print("Main called.")
     try:
-        edge_detection_service()
+        Edge_Detection_Service()
     except rospy.ROSInterruptException:
-        print("Error occured. Abort...")
-        rospy.logerr("Error")
+        rospy.logerr("Error occured. Abort...")
